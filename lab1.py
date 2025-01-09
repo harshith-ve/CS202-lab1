de8a8ffb72b88c9b90da49338d75d845e19f5456
harshith-ve <harshith.venigalla@iitgn.ac.in>
2025-01-09 10:42:21 +0530

    added comments

--- lab1.py
+++ lab1.py
@@ -53,7 +53,7 @@ def nth_fibonacci(index):
     return result_matrix[0][0] % MOD
 
 # Sample Input
-Nth_idx = 3
+NTH_IDX = 3
 
 # Print the nth Fibonacci number
-print(nth_fibonacci(Nth_idx))
+print(nth_fibonacci(NTH_IDX))
